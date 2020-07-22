306fa4c4d5cd13a29b26d8322136399289170492
Mariya <strangerloveless@yandex.ru>
2020-07-22 15:49:09 +0300

    Get rid of not needed components

--- frontend/src/app/modules/login/containers/login-screen/login-screen.component.ts
+++ frontend/src/app/modules/login/containers/login-screen/login-screen.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Store, select } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { increment, decrement, reset } from '../../store/counter/actions';
-import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-login-screen',
@@ -11,34 +8,18 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./login-screen.component.scss']
 })
 
-export class LoginScreenContainerСomponent implements OnInit {
-  count$: Observable<number>;
-  subscription: Subscription;
+export class LoginScreenContainerСomponent implements OnInit { 
+  username: string;
+  password: string;
 
   constructor(private store: Store<{ count: number }>) {
-    this.count$ = store.pipe(select('count'));
-  }
- 
-  increment() {
-    this.store.dispatch(increment());
-  }
- 
-  decrement() {
-    this.store.dispatch(decrement());
-  }
- 
-  reset() {
-    this.store.dispatch(reset());
   }
 
   ngOnInit(): void {
-    this.subscription = this.count$.subscribe();
+    var showSpinner: boolean = true;
   }
 
   ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
   }
 
 }
